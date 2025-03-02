8bf07b525c3761041d7c2bf3d8263a1616e6204f
Ericcccccai <102574544+Ericcccccai@users.noreply.github.com>
2025-03-02 00:53:19 -0800

    Update get_leftover_image_data.py

--- get_leftover_image_data.py
+++ get_leftover_image_data.py
@@ -23,14 +23,14 @@ options = Options()
 driver = webdriver.Chrome(options=options)
 
 #get all the tags to DPR
-driver.get("https://app.structionsite.com/projects/45482/photos")
+driver.get("https://app.structionsite.com/projects/xxx/photos")
 
 #enters user name
-driver.switch_to.active_element.send_keys("sunggoo@fieldai.com") #REPLACE EMAIL
+driver.switch_to.active_element.send_keys("xxx") #REPLACE EMAIL
 
 #enter password
 passwordSelector = driver.find_element(By.ID, "user_password")
-passwordSelector.send_keys("23361madero!") #REPLACE PASSWORD
+passwordSelector.send_keys("xxx") #REPLACE PASSWORD
 
 #click login button
 loginButton = driver.find_element(By.ID, "sign-in-button")
